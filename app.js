488eb91137eebfc6aeb123e612c7690ef2bee15f
Smooty Ziah <ehydlebeerg2@gmail.com>
2022-04-17 22:24:43 -0400

    new push

---
+++ app.js
@@ -0,0 +1,37 @@
+pink = 100
+blue = 200
+yellow = 300
+purple = 400
+ 
+function yellow (param1) {
+    console.log(param1);
+   return sum
+  }
+ yellow(300);
+
+ function blue (param1) {
+    console.log(param1);
+   return sum;
+  }
+ blue(200);
+
+ function pink (param1,param2) {
+    console.log(param1 * param2);
+   return sum;
+  }
+pink (100,200);
+
+function purple (param1,param2) {
+    console.log(param1 / param2);
+   return sum;
+  }
+purple (400,200);
+
+yellow => yellow + 300;
+blue => blue + 200;
+(pink, blue) => pink * blue + 100;
+(purple, blue) => purple / blue + 400;
+
+
+
+
